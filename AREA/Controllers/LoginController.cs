71e3bd81c6381e62b0fe7c2b0ab2ff6973249b9c
Simbrams <simon.brami@epitech.eu>
2017-11-13 17:35:48 +0100

    FIRST VERSION BASE DE PROJET AVEC LOGIN ET REGISTER CLASSIQUE

---
+++ AREA/Controllers/LoginController.cs
@@ -0,0 +1,75 @@
+﻿using AREA.Models;
+using AREA.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AREA.Controllers
+{
+    [Authorize]
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Index(VerifyLogin elem)
+        {
+            if (ModelState.IsValid)
+            {
+                using (AreaEntities db = new AreaEntities())
+                {
+                    bool UserValid = db.users.Any(data => data.Email == elem.Email && data.Password == elem.Password);
+                    if (UserValid)
+                    {
+                        FormsAuthentication.SetAuthCookie(elem.Email, false);
+                        return Redirect("/");
+                    }
+                }
+            }
+            return View(elem);
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+       [HttpPost]
+       [ValidateAntiForgeryToken]
+       public ActionResult Register(VerifyRegister elem)
+        {
+            if (ModelState.IsValid)
+            {
+                using (AreaEntities db = new AreaEntities())
+                {
+                    user ToAdd = new user()
+                    {
+                        Email = elem.Email,
+                        Name = elem.Name,
+                        Password = elem.Password
+                    };
+                    db.users.Add(ToAdd);
+                    db.SaveChanges();
+                    FormsAuthentication.SetAuthCookie(elem.Email, false);
+                    return Redirect("/login");
+                }
+            }
+            return View(elem);
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/");
+        }
+    }
+}
---
+++ AREA/Models/LoginModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AREA.Models.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AREA.Models
+{
+    public  class LoginModel
+    {
+        public void AddUser(user ToTest)
+        {
+            using (AreaEntities db = new AreaEntities())
+            {
+                user tmp = new user { Id = 1 };
+                db.users.Add(tmp);
+                db.SaveChanges();
+            }
+        }
+    }
+
+    public class VerifyRegister
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password combination is wrong")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class VerifyLogin
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
