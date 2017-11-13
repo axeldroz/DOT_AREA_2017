94a76a857ece7b37c86d0619ca11a736201c89e2
Simbrams <simon.brami@epitech.eu>
2017-11-13 20:47:31 +0100

    Strarting google auth

---
+++ AREA/App_Start/AuthConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Membership.OpenAuth;
+using DotNetOpenAuth.GoogleOAuth2;
+
+namespace AREA.App_Start
+{
+
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            GoogleOAuth2Client clientGoog = new GoogleOAuth2Client("271715026055-qd1vef8o8jf17ituksv477apsvbd66dm.apps.googleusercontent.com", "bgD4e0mvw-xp6iByc9KpGBrG");
+            IDictionary<string, string> extraData = new Dictionary<string, string>();
+            OpenAuth.AuthenticationClients.Add("google", () => clientGoog, extraData);
+        }
+    }
+
+}
